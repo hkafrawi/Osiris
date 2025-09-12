6287e9a9f26bb1df0299f7e05914a16b6d3ae111
Kafrawi <hassaballahelkafrawi@gmail.com>
2025-09-12 14:35:15 +0200

    Add: adding read, clean and load data for steamlit app

---
+++ app.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import pandas as pd
+import os
+
+
+# -------------------------------
+# Read, Clean and Load Data
+# -------------------------------
+def read_datafiles(directory):
+    data_files = []
+    for file in os.listdir(directory):
+        file_path = os.path.join(directory, file)
+        df = pd.read_csv(file_path)
+        data_files.append(df)
+
+    return data_files
+
+
+def clean_data(df, product_column_name, weight_column_name, price_column_name):
+    if product_column_name not in df.columns:
+        raise ValueError("Product name column does not exist in DataFrame")
+    if weight_column_name not in df.columns:
+        raise ValueError("Weight unit column does not exist in DataFrame")
+    if price_column_name not in df.columns:
+        raise ValueError("Price column does not exist in DataFrame")
+    df["Product_Name"] = df.apply(
+        lambda row: (
+            row[product_column_name]
+            if pd.isna(row[weight_column_name])
+            else f"{row[product_column_name]}_{row[weight_column_name]}"
+        ),
+        axis=1,
+    )
+    new_df = df[["Date", "Product_Name", price_column_name, "Source"]]
+    new_df.columns = ["Date", "Product_Name", "Price", "Source"]
+    return new_df
+
+
+@st.cache_data
+def load_data():
+    all_data = []
+    directories = {
+        "Seoudi_tags": [
+            "name",
+            "weight_base_unit",
+            "price_range.maximum_price.regular_price.value",
+        ],
+        "Carrefour_tags": ["name", "unit.unitOfMeasure", "price.price"],
+    }
+    for directory, items in directories.items():
+        all_dfs = read_datafiles(directory)
+        df = pd.concat(all_dfs)
+        cleaned_df = clean_data(df, *items)
+        all_data.append(cleaned_df)
+    final_df = pd.concat(all_data)
+    return final_df
+
+
+df = load_data()
